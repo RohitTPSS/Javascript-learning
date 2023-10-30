33d9c5ca00988e0cd556616876b0b19d322fa592
rohittpss <rohitbhandaritpss@gmail.com>
2023-10-30 09:13:11 +0530

    adf

--- 01-Basics/05-data-types-summary.js
+++ 01-Basics/05-data-types-summary.js
@@ -10,7 +10,6 @@ const anotherId = Symbol("123")
 console.log(id === anotherId) // False 
 
 const bigNumber = 123456789101112n // Bigint
-// test
 
 // Non-premitive datatypes ( Reference type )
 // types === array, Objects, functions  
