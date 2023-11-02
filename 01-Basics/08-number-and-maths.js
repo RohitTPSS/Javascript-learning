3a34d57ebf45691abb1f16ddc573f4fc199dd7f9
rohittpss <rohitbhandaritpss@gmail.com>
2023-11-02 10:22:05 +0530

    numbers and maths

---
+++ 01-Basics/08-number-and-maths.js
@@ -0,0 +1,41 @@
+// ////////////////// NUMBERS //////////////////
+
+// const score = 400
+// console.log(score)
+// console.log(typeof score)
+
+// // Another type to write for make this value number datatype 
+
+// const balance = new Number(1000)
+// console.log(balance)
+// console.log(typeof balance) // Object
+
+// console.log(balance.toString().length) // 4 output
+// console.log(balance.toFixed(2))  // 1000.00 output
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString()) // 10,000,000 output
+// console.log(hundreds.toLocaleString("en-IN")) // 1,00,00,000 output ( indian method )
+
+
+// ////////////////// MATHS //////////////////
+
+// console.log(Math)
+// console.log(typeof Math) // Object
+
+// console.log(Math.abs(-5)) // its converts negative value to positive 
+// console.log(Math.round(4.4)) // its get  round value ( output 4 )
+// console.log(Math.ceil(4.2)) // it will always get higher value ( output = 5 )
+// console.log(Math.floor(4.9)) // it will always get lower value ( output = 4 )
+// console.log(Math.min(5, 4, 8, 9)) // it will get minimum value from this array ( output = 4 )
+// console.log(Math.max(5, 4, 8, 9)) // it will get maximum value from this array ( output = 9 )
+
+// console.log(Math.random())
+// console.log(Math.random() * 10 + 1) // we add + 1 because of avoid output 0
+// console.log((Math.random() * 10) + 1)
+// console.log(Math.ceil((Math.random() * 10)) + 1)
+
+// const minValue = 10
+// const maxValue = 20
+
+// console.log(Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue) // it will make output between 10 to 20
