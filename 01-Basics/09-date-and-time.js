30886addaf4c2877784072f898641bf96b0068b5
rohittpss <rohitbhandaritpss@gmail.com>
2023-11-30 14:11:27 +0530

    latest update on date and time

---
+++ 01-Basics/09-date-and-time.js
@@ -0,0 +1,14 @@
+//////// DATE /////////
+
+// Jan 1 1970, UTC ( beginning of date start from )
+
+let myDate = new Date()
+console.log(myDate)
+console.log(typeof myDate)  // Object
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleString())
+
+let myCreatedDate = new Date("")
+console.log(myCreatedDate.toDateString)
